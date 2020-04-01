358acb9f7f3fa152d39785730e3b0b3929e67c77
Salvatore Lauriola <salvatore6324@gmail.com>
2020-04-01 16:47:58 +0200

    finished.

--- script/main.js
+++ script/main.js
@@ -1,19 +1,20 @@
 // 1 Stampare i numeri da 1 a 100
 
-
-
 for (var i=0; i <= 100; i++) {
-  
-
-  if((i % 3 == 0 ) && (i % 5 != 0)){
-    console.log(i + 'Fizz');
-  }else if((i % 5 == 0) && (i % 3 != 0)) {
-    console.log(i + 'Buzz');
-
-  }else if ((i % 3 == 0 ) && (i % 5 == 0)) {
+  if ((i % 3 == 0 ) && (i % 5 == 0)) {
     console.log(i + 'FizzBuzz');
+
+    // Per i multipli di 3 stampi Fizz al posto del numero
+
+  }else if (i % 3 == 0 ) {
+    console.log('Fizz')
+
+    //  Per i multipli di 5 stampi Buzz.
+  
+  } else if(i % 5 == 0) {
+    console.log(i + 'Buzz')
+  
   }else {
     console.log(i);
   }
 }
- 
