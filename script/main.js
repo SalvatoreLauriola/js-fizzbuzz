acbe8481e969b264ceda13f4b68b7e7d14b5a01f
Salvatore Lauriola <salvatore6324@gmail.com>
2020-04-01 15:52:03 +0200

    Ok in console.

---
+++ script/main.js
@@ -0,0 +1,19 @@
+// 1 Stampare i numeri da 1 a 100
+
+
+
+for (var i=0; i <= 100; i++) {
+  
+
+  if((i % 3 == 0 ) && (i % 5 != 0)){
+    console.log(i + 'Fizz');
+  }else if((i % 5 == 0) && (i % 3 != 0)) {
+    console.log(i + 'Buzz');
+
+  }else if ((i % 3 == 0 ) && (i % 5 == 0)) {
+    console.log(i + 'FizzBuzz');
+  }else {
+    console.log(i);
+  }
+}
+ 
